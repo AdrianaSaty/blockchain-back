10a53f1df3095b970f99c2c755d2bc8a010ae1ae
Adriana Saty Bertelli <Adria@adria-2.partner.bs.br.bsch>
2019-11-27 09:56:24 -0300

    <feat> proof of work construction

--- blockchain.js
+++ blockchain.js
@@ -32,15 +32,18 @@ class Blockchain {
   static isValidChain(chain) {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) return false;
     for (let i = 1; i < chain.length; i++) {
-      const { timestamp, lastHash, hash, data } = chain[i];
+      const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
 
       const actualLastHash = chain[i - 1].hash;
+      const lastDifficulty = chain[i-1].difficulty;
 
       if (lastHash !== actualLastHash) return false;
 
-      const validatedHash = cryptoHash(timestamp, lastHash, data);
+      const validatedHash = cryptoHash(timestamp, lastHash, nonce, difficulty, data);
 
       if (hash !== validatedHash) return false;
+
+      if((lastDifficulty - difficulty) > 1) return false;
     }
     return true;
   }
--- config.js
+++ config.js
@@ -1,8 +1,13 @@
+const MINE_RATE = 1000; //its 1 second
+const INICIAL_DIFFICULTY = 3;
+
 const GENESIS_DATA = {
   timestamp: 1,
   lastHash: '-----',
   hash: 'hash-one',
+  difficulty: INICIAL_DIFFICULTY,
+  nonce: 0,
   data: [],
 };
 
-module.exports = { GENESIS_DATA };
+module.exports = { GENESIS_DATA, MINE_RATE };
