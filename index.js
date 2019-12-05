2b5d5e5b6b24093d672b34fa2be23ea66754ec1b
Adriana Saty Bertelli <Adria@adria-2.partner.bs.br.bsch>
2019-12-05 11:55:58 -0300

    <feat> added cors

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PubSub = require('./app/pubsub');
 const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner');
+const cors = require('cors');
 
 const isDevelopment = process.env.ENV === 'development';
 
@@ -27,6 +28,12 @@ const transactionMiner = new TransactionMiner({ blockchain, transactionPool, wal
 
 
 app.use(bodyParser.json());
+
+app.use(cors({
+    // origin: process.env.ENV === "DSV" ? 'http://localhost:3000' : "https://blockchain-back-login.herokuapp.com/",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  }));
+
 
 app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
